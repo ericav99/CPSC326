35e215c6c465cdf6fccd7c127d6b9e94c18438d6
Qwertie64982 <maxwell.a.sherman@live.com>
2019-03-19 23:30:50 -0700

    Add test script

---
+++ HW5/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Test 01"
+./hw5.py Testing/hw5_t01.mypl > Testing/hw5_r01.txt 2>&1
+echo "Test 02"
+./hw5.py Testing/hw5_t02.mypl > Testing/hw5_r02.txt 2>&1
+echo "Test 03"
+./hw5.py Testing/hw5_t03.mypl > Testing/hw5_r03.txt 2>&1
+echo "Test 04"
+./hw5.py Testing/hw5_t04.mypl > Testing/hw5_r04.txt 2>&1
