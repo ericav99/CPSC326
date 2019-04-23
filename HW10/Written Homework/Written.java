1cd2920bfaa5690e8a0dfd97b6d559417cc88bfc
Qwertie64982 <maxwell.a.sherman@live.com>
2019-04-22 22:06:59 -0700

    Add Java higher order functions

--- HW10/Written Homework/Written.java
+++ HW10/Written Homework/Written.java
@@ -4,10 +4,50 @@
  * Assignment: 10
  * Description:
  *   Higher order function examples in Java
- */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Function;
 
 public class Written {
-    public static void main(String args[]) {
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        Function<Integer, Integer> isEven = x -> x % 2 == 0 ? 1 : 0;
+        Function<Integer, Integer> addOne = x -> x + 1;
+
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            l.add(i);
+        }
+
+        // check if even (1 = true, 0 = false)
+        ArrayList<Integer> l2 = myMap(isEven, l);
+        for (int item : l2) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        // empty list
+        l2 = myMap(addOne, new ArrayList<Integer>());
+        for (int item : l2) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        // add 1
+        l2 = myMap(addOne, l);
+        for (int item : l2) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+
+    }
+
+    public static ArrayList<Integer> myMap(Function<Integer, Integer> func, ArrayList<Integer> lst) {
+        ArrayList<Integer> newLst = new ArrayList<>();
+        for (int item : lst) {
+            newLst.add(func.apply(item));
+        }
+        return newLst;
     }
 }
